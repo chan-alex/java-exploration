0325936bdc89571b81f162a3ff0e435a8e3fa40f
alex chan <chan.k.leong@gmail.com>
2017-03-20 08:23:57 +0800

    added new project

---
+++ system_properties/src/main/java/com/chanalex/App.java
@@ -0,0 +1,17 @@
+package com.chanalex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Start:" );
+
+        String name = System.getProperty("name");
+
+        System.out.println("Hello " + name);
+    }
+}
