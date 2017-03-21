9293a7f755688ed7751b5d43a0fa480c100533a1
alex chan <chan.k.leong@gmail.com>
2017-03-21 08:12:29 +0800

    Added java_basics1

---
+++ java_basics1/src/main/java/com/chanalex/App.java
@@ -0,0 +1,27 @@
+package com.chanalex;
+
+
+import java.io.BufferedReader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+        System.out.println( "Start" );
+
+        test1();
+    }
+
+
+    public static void test1( ) {
+
+        System.out.println("test1");
+
+        BufferedReader in;
+
+    }
+}
