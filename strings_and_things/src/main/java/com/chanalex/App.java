d2108d2a0ee55069b68954ccbb7fa518b388d210
alex chan <chan.k.leong@gmail.com>
2017-03-24 08:06:05 +0800

    Added stuff to App.java

--- strings_and_things/src/main/java/com/chanalex/App.java
+++ strings_and_things/src/main/java/com/chanalex/App.java
@@ -1,5 +1,8 @@
 package com.chanalex;
 
+
+import java.util.StringTokenizer;
+
 /**
  * Hello world!
  *
@@ -9,18 +12,44 @@ public class App {
 
         System.out.println("Start");
 
+        taking_apart_strings();
+        breaking_string_into_words();
+
     }
 
     public static void taking_apart_strings() {
 
-        System.out.println("From taking_apart_strings: ");
+        // using String substring() method
+        System.out.println("From taking_apart_strings(): ");
         String str = "Java has a lot of strings.";
 
-        System.out.println("Starting str =" + str);
+        System.out.println("Starting str =" + str + "\n");
 
         System.out.println("str.substring(5) = " + str.substring(5));
+        System.out.println("str.substring(5) = " + str.substring(7,12));
 
 
+    }
+
+    public static void breaking_string_into_words() {
+
+        System.out.println("\nFrom breaking_string_into_words(): ");
+        String str = "Java has a lot of strings.";
+
+
+        // Easy way is to use the .split method.
+        // for(String word: str.split(" ")) {
+        //     System.out.print(word);
+        // }
+
+        // Use java.util.StringTokenizer
+        // By default, break as standard word boundaries.
+        StringTokenizer st = new StringTokenizer(str);
+        while (st.hasMoreTokens()) {
+            System.out.println("Token = " + st.nextToken());
+        }
+
+        
     }
 }
 
