94ad96d7c4f6c60edb1c1f190fd44745b109e80b
alex chan <chan.k.leong@gmail.com>
2017-04-10 08:08:23 +0800

    Added stuff to generic2

--- generic2/src/main/java/com/chanalex/App.java
+++ generic2/src/main/java/com/chanalex/App.java
@@ -2,34 +2,37 @@ package com.chanalex;
 import java.util.*;
 
 /**
- * Start!
+ * Start
  *
  */
 
 
 interface Shape {
-
     double area();
 }
 
 class Circle implements Shape {
     static double PI = 3.14;
     double radius = 0.0;
+
     Circle (double r) { this.radius = r; }
+
     public double area() { return PI * radius * radius;}
 }
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        System.out.println("Start:");
+    
+        // The right using diamond syntax "<>".
+        // Compiler infers type from left side.
+        List<Shape> shapes1 = new ArrayList<>();
+
+        Circle c1 = new Circle(10.0);
+        shapes1.add(c1);
+        System.out.println(shapes1);
+
     }
-
-    List<Shape> shapes1 = new ArrayList<>();
-
-    Circle c1 = new Circle(10.0);
-    System.out.println(shapes1);
-    //shapes.add(c1);
-
 }
