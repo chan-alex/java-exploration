7bdd100836b0280e5c7873c4799cefd43bd56f17
alex chan <chan.k.leong@gmail.com>
2017-04-13 08:17:55 +0800

    Added more stuff to exceptions1

--- exceptions1/src/main/java/com/chanalex/App.java
+++ exceptions1/src/main/java/com/chanalex/App.java
@@ -1,5 +1,8 @@
 package com.chanalex;
-import java.io.File;
+import java.io.*;
+
+import java.util.zip.ZipException;
+import javax.net.ssl.SSLException;
 /**
  * Start!
  *
@@ -10,12 +13,35 @@ public class App
 
         System.out.println( "Start:" );
 
-        file_status("/etc/hosts");
+        // Because the open_file() throws exceptions, It
+        // must be handled in the try/catch block.
+        try {
 
-        file_status("/etc/does_not_exist");
+            open_file("/etc/hosts");
+            open_file("/etc/does_not_exist");
+            System.out.println("Code should not reach here.");
 
+        } catch ( FileNotFoundException e) {
+
+            System.out.println(e);
+        }
+        //   1) New in java 7: catch mutiple exeception in a catch clause
+        //   2) Java also does not allow catching exceptions that has no possbility for being thrown.
+        //} catch ( ZipException | SSLException e ) {
+           // System.out.println("Probably should not reach here. Just for demo purposes.");
+        //}
+        finally {
+
+            // All code in the finally clause are guarenteed to executed no matter
+            // how control leaves the try clause.
+            System.out.println("Calling from the finally block.");
+
+        }
+
+        System.out.println("End.");
     }
 
+    // This method does not throws any exceptions.
     public static void file_status(String filename) {
 
         System.out.println("From file_method method:");
@@ -31,8 +57,13 @@ public class App
             System.out.println("It does not exist");
         }
 
+    }
 
+    // This method calls codes that throws FileNotFoundException exceptions
+    // and so must have a "throws" clause in the declaration.
+    public static void open_file(String filename) throws FileNotFoundException {
 
+        BufferedReader is = new BufferedReader(new FileReader(filename));
 
     }
 }
